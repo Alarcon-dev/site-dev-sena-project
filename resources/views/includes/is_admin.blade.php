5594d640e9e9fa1ebc19c95c150383b718841b51
alarcon steven <devalarcon.23@gmail.com>
2024-02-27 15:05:13 -0500

    configuraciones de sidebar

---
+++ resources/views/includes/is_admin.blade.php
@@ -0,0 +1,27 @@
+<li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+        <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+        <div class="d-sm-none d-lg-inline-block">Aministrador {{ Auth::user()->user_name }}</div>
+    </a>
+    <div class="dropdown-menu dropdown-menu-right">
+        <div class="dropdown-title login_time"></div>
+        <a href="features-profile.html" class="dropdown-item has-icon">
+            <i class="far fa-user"></i> Perfil
+        </a>
+        <a href="features-activities.html" class="dropdown-item has-icon">
+            <i class="fas fa-bolt"></i> Consultar usuarios
+        </a>
+        <a href="features-settings.html" class="dropdown-item has-icon">
+            <i class="fas fa-cog"></i> Actulizar datos
+        </a>
+        {{-- <div class="dropdown-divider"></div> --}}
+        <a href="{{ route('logout') }}" class="dropdown-item has-icon text-danger"
+            onclick="event.preventDefault();
+    document.getElementById('logout-form').submit();">
+            <i class="fas fa-sign-out-alt"></i> Cerrar sessión
+        </a>
+        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+            @csrf
+        </form>
+
+    </div>
+</li>
